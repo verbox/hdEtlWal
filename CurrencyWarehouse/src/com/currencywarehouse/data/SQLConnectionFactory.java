44e381b2819c2b27032b51dc3bac91a4fe4b3222
Michal Szczygiel <mychal.szczygiel@gmail.com>
2013-01-07 18:36:41 +0100

    - Added factory for SqlConnection - to be implemented

---
+++ CurrencyWarehouse/src/com/currencywarehouse/data/SQLConnectionFactory.java
@@ -0,0 +1,11 @@
+package com.currencywarehouse.data;
+
+import java.sql.Connection;
+
+public class SQLConnectionFactory {
+	public Connection createConnection()
+	{
+		//TODO: return correct connection to database.
+		return null;
+	}
+}
