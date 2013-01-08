17ac5b735034a002cdde0719210fe10cdf49ca58
Michal Szczygiel <mychal.szczygiel@gmail.com>
2013-01-08 11:26:02 +0100

    - Transactional DataInserter interface

--- CurrencyWarehouse/src/com/currencywarehouse/data/DataInserter.java
+++ CurrencyWarehouse/src/com/currencywarehouse/data/DataInserter.java
@@ -3,5 +3,14 @@ package com.currencywarehouse.data;
 import com.currencywarehouse.entities.Currency;
 
 public interface DataInserter {
+	/**
+	 * Begins new transaction
+	 */
+	public void beginInsert();
 	public void insertCurrencyHistoryEntry(Currency currency);
+	
+	/**
+	 * Ends current transaction
+	 */
+	public void endInsert();
 }
